be0b2f1ee49db7991fbe605e0f783301578a63d5
Vijay Stroup <vijaystroup@gmail.com>
2022-08-10 19:46:17 -0400

    disable trpc batching

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppType } from 'next/dist/shared/lib/utils'
 import type { AppRouter } from '../server/routers/app'
 import { withTRPC } from '@trpc/next'
+import { httpLink } from '@trpc/client/links/httpLink'
 import '../styles/global.css'
 
 const MyApp: AppType = ({ Component, pageProps }) => {
@@ -10,8 +11,14 @@ const MyApp: AppType = ({ Component, pageProps }) => {
 export default withTRPC<AppRouter>({
   config({ ctx }) {
     const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
-      : 'http://localhost:3000/api/trpc'
+      ? `https://${process.env.VERCEL_URL}/api`
+      : 'http://localhost:3000/api'
+
+    const links = [
+      httpLink({
+        url
+      })
+    ]
 
     return {
       queryClientConfig: {
@@ -30,7 +37,7 @@ export default withTRPC<AppRouter>({
         }
         return {}
       },
-      url
+      links
     }
   },
   ssr: true
---
+++ src/pages/api/[trpc].ts
@@ -0,0 +1,18 @@
+import * as trpcNext from '@trpc/server/adapters/next'
+import { createContext } from '../../server/context'
+import { appRouter } from '../../server/routers/app'
+
+export default trpcNext.createNextApiHandler({
+  router: appRouter,
+  createContext,
+  batching: {
+    enabled: false
+  },
+  onError({ error }) {
+    if (error.code === 'INTERNAL_SERVER_ERROR') {
+      console.error('Something went wrong', error)
+    } else {
+      console.error(error)
+    }
+  }
+})
