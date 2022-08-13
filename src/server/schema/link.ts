f8ca5c42aabc9ab59fb947386a8f520043b73eb1
Vijay Stroup <vijaystroup@gmail.com>
2022-08-13 18:40:36 -0400

    regex match for newLink schema

--- src/server/schema/link.ts
+++ src/server/schema/link.ts
@@ -1,5 +1,9 @@
 import { z } from 'zod'
 
 export const newLink = z.object({
-  destination: z.string().url()
+  destination: z
+    .string()
+    .regex(
+      /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/
+    )
 })
