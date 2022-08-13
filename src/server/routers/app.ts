18d264de014a5afb9d2edecd69c442cd1ca72044
Vijay Stroup <vijaystroup@gmail.com>
2022-08-13 16:48:56 -0400

    cleanup user route

--- src/server/routers/app.ts
+++ src/server/routers/app.ts
@@ -1,9 +1,6 @@
 import { createRouter } from '../createRouter'
-import { userRouter } from './user'
 import { linkRouter } from './link'
 
-export const appRouter = createRouter()
-  .merge('user.', userRouter)
-  .merge('link.', linkRouter)
+export const appRouter = createRouter().merge('link.', linkRouter)
 
 export type AppRouter = typeof appRouter
