63c385a74930de67b54bf6c9a62a740ad839675e
Vijay Stroup <vijaystroup@gmail.com>
2022-08-13 16:57:14 -0400

    Header with link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,16 @@
 import type { FC } from 'react'
+import Link from 'next/link'
 
 const Header: FC = () => {
   return (
     // TODO: add next-seo
 
     <header>
-      <h1 className='p-2 text-4xl text-blue-700'>to</h1>
+      <h1 className='p-2 text-4xl text-blue-700'>
+        <Link href='/'>
+          <a>to</a>
+        </Link>
+      </h1>
     </header>
   )
 }
