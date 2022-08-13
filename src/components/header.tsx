ead6e9c250839d5325654ae9c8cad1e0e548e05b
Vijay Stroup <vijaystroup@gmail.com>
2022-08-13 13:39:28 -0400

    Header component

---
+++ src/components/header.tsx
@@ -0,0 +1,13 @@
+import type { FC } from 'react'
+
+const Header: FC = () => {
+  return (
+    // TODO: add next-seo
+
+    <header>
+      <h1 className='p-2 text-4xl text-blue-700'>to</h1>
+    </header>
+  )
+}
+
+export default Header
