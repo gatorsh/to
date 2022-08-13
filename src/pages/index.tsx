e71ce798edb05f0569b7f282594925921bd96d51
Vijay Stroup <vijaystroup@gmail.com>
2022-08-13 16:57:21 -0400

    error styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,21 +4,28 @@ import { trpc } from '../utils/trpc'
 import {
   LinkIcon,
   ClipboardCopyIcon,
-  ClipboardCheckIcon
+  ClipboardCheckIcon,
+  CheckIcon,
+  XIcon
 } from '@heroicons/react/solid'
 import copy from 'copy-to-clipboard'
-import Header from '../components/header'
-import Footer from '../components/footer'
+import Spinner from '../components/spinner'
 
 const Home: NextPage = () => {
+  const linkClassAttr = 'w-5 h-5'
+
   const [destination, setDestination] = useState('')
-  const [slug, setSlug] = useState<string | null>('asdf1')
+  const [slug, setSlug] = useState<string | null>(null)
   const [error, setError] = useState<string | null>(null)
-  const [coppied, setCoppied] = useState(false)
+  const [copied, setCopied] = useState(false)
+  const [linkIcon, setLinkIcon] = useState(
+    <LinkIcon className={linkClassAttr} />
+  )
 
   const { mutate } = trpc.useMutation(['link.new'])
 
   const submit = useCallback(() => {
+    setLinkIcon(<Spinner />)
     mutate(
       {
         destination
@@ -27,77 +34,83 @@ const Home: NextPage = () => {
         onSuccess: (slug) => {
           setError(null)
           setSlug(slug)
+          setLinkIcon(<CheckIcon className={linkClassAttr} />)
         },
         onError: (e) => {
           if (e.data?.code === 'CONFLICT') {
             // TODO: retry
           }
 
-          setError(e.message)
+          const error = JSON.parse(e.message)[0]
+
+          setError(error.message)
+          setLinkIcon(<XIcon className={linkClassAttr + ' text-red-600'} />)
         }
       }
     )
   }, [destination, slug])
 
   return (
-    <>
-      <Header />
+    <main className='flex flex-col items-center mt-24'>
+      <div>
+        <label htmlFor='destination' className='text-xs text-slate-500'>
+          URL To Shorten
+        </label>
+        <div className='flex justify-center'>
+          <input
+            type='input'
+            id='destination'
+            className={`px-2.5 text-md bg-transparent border backdrop-blur-[1.5px] rounded-l-lg placeholder-slate-300 ${
+              error ? 'border-red-600' : ''
+            }`}
+            placeholder='https://www.gator.sh/...'
+            required
+            value={destination}
+            onChange={(e) => {
+              setDestination(e.currentTarget.value)
+              if (error) {
+                setError(null)
+                setLinkIcon(<LinkIcon className={linkClassAttr} />)
+              }
+            }}
+          />
+          <button
+            type='submit'
+            className='p-2.5 text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800'
+            onClick={submit}
+          >
+            {linkIcon}
+          </button>
+        </div>
+      </div>
 
-      <main className='flex flex-col items-center mt-24'>
-        <div className=''>
-          <label htmlFor='destination' className='text-xs text-slate-500'>
-            URL To Shorten
-          </label>
-          <div className='flex justify-center'>
-            <input
-              type='input'
-              id='destination'
-              className='px-2.5 text-md bg-transparent border backdrop-blur-[1.5px] rounded-l-lg placeholder-slate-300'
-              placeholder='https://www.gator.sh/...'
-              required
-              value={destination}
-              onChange={(e) => setDestination(e.currentTarget.value)}
-            />
+      {error && <p className='text-red-600'>{error}</p>}
+
+      {slug && (
+        <div className='flex flex-col mt-10'>
+          <p className='text-xs text-slate-500'>Shortened URL</p>
+          <div className='flex'>
+            <span className='inline-flex items-center px-2.5 text-md text-slate-400 bg-slate-300 rounded-l-lg'>
+              to.gator.sh/
+            </span>
+            <span className='px-2.5 bg-transparent border backdrop-blur-[1.5px] text-blue-700 flex items-center text-lg'>
+              {slug}
+            </span>
             <button
               type='submit'
               className='p-2.5 text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800'
-              onClick={submit}
+              onClick={() => {
+                copy(`http://localhost:3000/${slug}`)
+                setCopied(true)
+              }}
             >
-              <LinkIcon className='w-5 h-5' />
+              {copied && <ClipboardCheckIcon className='w-5 h-5' />}
+              {!copied && <ClipboardCopyIcon className='w-5 h-5' />}
             </button>
           </div>
         </div>
-
-        {slug && (
-          <div className='flex flex-col mt-10'>
-            <p className='text-xs text-slate-500'>Shortened URL</p>
-            <div className='flex'>
-              <span className='inline-flex items-center px-2.5 text-md text-slate-400 bg-slate-300 rounded-l-lg'>
-                to.gator.sh/
-              </span>
-              <span className='px-2.5 bg-transparent border backdrop-blur-[1.5px] text-blue-700 flex items-center text-lg'>
-                {slug}
-              </span>
-              <button
-                type='submit'
-                className='p-2.5 text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800'
-                onClick={() => {
-                  copy(`http://localhost:3000/${slug}`)
-                  setCoppied(true)
-                }}
-              >
-                {coppied && <ClipboardCheckIcon className='w-5 h-5' />}
-                {!coppied && <ClipboardCopyIcon className='w-5 h-5' />}
-              </button>
-            </div>
-          </div>
-        )}
-
-        {error && <p>{error}</p>}
-      </main>
-
-      <Footer />
-    </>
+      )}
+    </main>
   )
 }
 
