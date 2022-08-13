da454e8ea1ee54d2884bb797adfb74dcec815de3
Vijay Stroup <vijaystroup@gmail.com>
2022-08-13 18:58:36 -0400

    back to url

--- src/server/schema/link.ts
+++ src/server/schema/link.ts
@@ -1,9 +1,10 @@
 import { z } from 'zod'
 
 export const newLink = z.object({
-  destination: z
-    .string()
-    .regex(
-      /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/
-    )
+  destination: z.string().url()
+
+  // TODO: use regex as validation
+  // .regex(
+  //   /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/
+  // )
 })
