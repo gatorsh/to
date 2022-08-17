d740ea73e83e2e8f2c7afa543d9a3caa18135f2a
Vijay Stroup <vijaystroup@gmail.com>
2022-08-17 12:39:01 -0400

    regex schema instead of built-in z.url

--- src/server/schema/link.ts
+++ src/server/schema/link.ts
@@ -1,10 +1,9 @@
 import { z } from 'zod'
 
 export const newLink = z.object({
-  destination: z.string().url()
-
-  // TODO: use regex as validation
-  // .regex(
-  //   /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/
-  // )
+  destination: z
+    .string()
+    .regex(
+      /^((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/
+    )
 })
